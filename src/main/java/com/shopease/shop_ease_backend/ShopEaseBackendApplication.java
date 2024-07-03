6c68d4f02ffd8317087fec63e96ab0b7acec8375
ss35789 <ss35789@naver.com>
2024-07-03 15:12:50 +0900

    Initial commit

---
+++ src/main/java/com/shopease/shop_ease_backend/ShopEaseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shopease.shop_ease_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopEaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopEaseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopease/shop_ease_backend/ShopEaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopease.shop_ease_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopEaseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
