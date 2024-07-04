fc33f34c5817bf81e492f04746b1ea618a85077c
ss35789 <ss35789@naver.com>
2024-07-04 18:47:34 +0900

    Create Category.java

---
+++ src/main/java/com/shopease/domain/Category.java
@@ -0,0 +1,22 @@
+package com.shopease.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter@Setter
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryKey;
+
+    @ManyToMany
+    private List<Item> items;
+
+    @Column(nullable = false)
+    private String name;
+}
