f5208e1f685719eb0c44a1495d8f577df3cb1d0c
ss35789 <ss35789@naver.com>
2024-07-06 18:30:24 +0900

    Controller의 비지니스 코드 분리
    Service로  로그인 코드 옮김
    순환 참조 발생 ->
    UserDetailService 를 따로 만들어서 해결

--- src/main/java/com/shopease/shop_ease_backend/service/UserService.java
+++ src/main/java/com/shopease/shop_ease_backend/service/UserService.java
@@ -1,42 +1,51 @@
 package com.shopease.shop_ease_backend.service;
 
 import com.shopease.shop_ease_backend.domain.User;
+import com.shopease.shop_ease_backend.dto.LoginRequest;
 import com.shopease.shop_ease_backend.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class UserService implements UserDetailsService {
+public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException { // 로그인
-        User user = userRepository.findByEmail(email);
-
-        if (user == null) {
-            throw new UsernameNotFoundException("User not found");
-        }
-
-        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
-    }
+    private final AuthenticationManager authenticationManager;
 
     public User save(User user) { // 회원가입
         user.setPassword(passwordEncoder.encode(user.getPassword())); // password 암호화
         return userRepository.save(user);
     }
 
-    public User findByEmail(String email){
+    public User userLogin(LoginRequest loginRequest) throws Exception {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword())
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        User user = userRepository.findByEmail(loginRequest.getEmail());
+        if (user == null) {
+            throw new Exception("Invalid email or password");
+        }
+        return user;
+    }
+
+    public void registerUser(User user) throws Exception{
+        if(findByEmail(user.getEmail()) == null){
+            throw new Exception("Email is already in use");
+        }
+        save(user);
+    }
+
+    public User findByEmail(String email) {
         return userRepository.findByEmail(email);
     }
 }
