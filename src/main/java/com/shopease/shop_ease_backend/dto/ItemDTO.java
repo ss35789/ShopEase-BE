1d81ebc2185017fcc4e09d17e7ca938a4a503c93
ss35789 <ss35789@naver.com>
2024-07-18 17:44:45 +0900

    .

---
+++ src/main/java/com/shopease/shop_ease_backend/dto/ItemDTO.java
@@ -0,0 +1,46 @@
+package com.shopease.shop_ease_backend.dto;
+
+import com.shopease.shop_ease_backend.domain.Category;
+import jakarta.persistence.*;
+        import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class ItemDTO {
+
+    private Long itemKey;
+
+    private List<Category> categories;
+
+    @NotBlank(message = "Name cannot be blank")
+    private String name;
+
+    @NotNull(message = "Price cannot be null")
+    private Integer price;
+
+    private Integer salePrice;
+
+    private String content;
+
+    private LocalDateTime uploadDate = LocalDateTime.now();
+
+    private String img;
+
+    public ItemDTO() {}
+
+    public ItemDTO(Long itemKey, String name, Integer price, Integer salePrice, String content, LocalDateTime uploadDate, String img) {
+        this.itemKey = itemKey;
+        this.name = name;
+        this.price = price;
+        this.salePrice = salePrice;
+        this.content = content;
+        this.uploadDate = uploadDate;
+        this.img = img;
+    }
+
+}
