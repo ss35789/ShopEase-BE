f49326fce12338d04dada8725e5706e4a65c2de1
ss35789 <ss35789@naver.com>
2024-07-18 17:36:53 +0900

    deleteItem()

--- src/main/java/com/shopease/shop_ease_backend/service/ItemService.java
+++ src/main/java/com/shopease/shop_ease_backend/service/ItemService.java
@@ -2,6 +2,7 @@ package com.shopease.shop_ease_backend.service;
 
 import com.shopease.shop_ease_backend.domain.Item;
 import com.shopease.shop_ease_backend.dto.ItemDTO;
+import com.shopease.shop_ease_backend.exception.ItemNotFoundException;
 import com.shopease.shop_ease_backend.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,14 @@ public class ItemService {
         Item savedItem = itemRepository.save(item);
         return convertToDTO(savedItem);
     }
+
+    public ItemDTO delItem(Long itemKey) {
+        Item existitem = itemRepository.findById(itemKey)
+                .orElseThrow(() -> new ItemNotFoundException("Item not found with key: " + itemKey));
+        itemRepository.delete(existitem);
+        return convertToDTO(existitem);
+    }
+
 
     private ItemDTO convertToDTO(Item item) {
         return new ItemDTO(
