d445aab8ebb3326fda037b3ddce82b7838e0ef25
ss35789 <ss35789@naver.com>
2024-07-18 17:53:33 +0900

    delete Item

---
+++ src/main/java/com/shopease/shop_ease_backend/controller/ItemController.java
@@ -0,0 +1,35 @@
+package com.shopease.shop_ease_backend.controller;
+
+import com.shopease.shop_ease_backend.dto.ItemDTO;
+import com.shopease.shop_ease_backend.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/items")
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping
+    public ResponseEntity<List<ItemDTO>> getAllItems() {
+        List<ItemDTO> items = itemService.getItems();
+        return ResponseEntity.ok(items);
+    }
+
+    @PostMapping
+    public ResponseEntity<ItemDTO> addItem(@RequestBody ItemDTO itemDTO) {
+        ItemDTO createdItem = itemService.addItem(itemDTO);
+        return ResponseEntity.ok(createdItem);
+    }
+
+    @DeleteMapping("/{itemKey}")
+    public ResponseEntity<ItemDTO> delItem(@PathVariable Long itemKey){
+        ItemDTO deleteItem = itemService.delItem(itemKey);
+        return ResponseEntity.ok(deleteItem);
+    }
+}
